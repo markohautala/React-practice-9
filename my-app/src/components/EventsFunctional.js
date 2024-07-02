0fd2a0cdf5279d40be20a7f2442fc05a0e8dfeb5
markohautala <markohautala@live.se>
2024-07-02 16:38:12 +0300

    Add more components

---
+++ my-app/src/components/EventsFunctional.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function EventsFunctional() {
+  function clickHandler() {
+    console.log('Button clicked - functional component')
+  }
+  
+  return (
+    <div>
+      <button onClick={clickHandler}>Click me - functional component</button>
+    </div>
+  )
+}
+
+export default EventsFunctional
